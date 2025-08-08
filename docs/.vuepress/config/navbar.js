093f4f8c376a2eacfbd9d7c58e6e949db5a2e985
zrx <ruixiang0430@126.com>
2025-08-08 21:14:00 +0800

    Updates

---
+++ docs/.vuepress/config/navbar.js
@@ -0,0 +1,7 @@
+export const navbar = [
+    { text: '主页', link: '/' },
+    { text: '指南', link: '/guide/getingStart' },
+    { text: 'API', link: '/API/APIUsing' },
+
+    { text: '主站', link: 'https://v2.vuepress.vuejs.org/zh' }
+]
